fbb67a61ce5fb902a4c907f0646a300fc6a25395
Fonzy57 <traben@hotmail.fr>
2025-04-13 11:12:23 +0200

    Add annotation for Dog model

--- src/main/java/com/k9club/api/model/Dog.java
+++ src/main/java/com/k9club/api/model/Dog.java
@@ -1,17 +1,36 @@
 package com.k9club.api.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Entity
 public class Dog {
 
-  protected int id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  protected Integer id;
+
+  @Column(nullable = false, length = 100)
   protected String name;
-  protected Date birthday;
+
+  @Column(nullable = false)
+  protected LocalDate birthday;
+
+  // TODO NULLABLE SI JAMAIS CHOIX 'AUTRE FAIT'
+  @Column(nullable = true)
   protected String gender;
-  protected Date createdAt;
+
+  @Column(nullable = false)
+  protected LocalDateTime createdAt;
+
+  // TODO ICI A NE PAS RENTRER DANS LA BDD
+  // CAR CALCULER A PARTIR DE LA DATE DE NAISSANCE
+  @Transient
+  protected int age; // TODO PAS SUR DU int CAR SI MOINS D'UN AN CA POSE PROBLÈME
 }
