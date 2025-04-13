83a7a6a5573e066b8732dd0764fe62d48990efc3
Fonzy57 <traben@hotmail.fr>
2025-04-13 10:26:14 +0200

    Add DogController

---
+++ src/main/java/com/k9club/api/controller/DogController.java
@@ -0,0 +1,34 @@
+package com.k9club.api.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class DogController {
+
+  @GetMapping("/dogs")
+  public String dogsList() {
+    return "Dogs List";
+  }
+
+  @GetMapping("/dog/{id}")
+  public String dog(@PathVariable int id) {
+    return "Dog with ID : " + id;
+  }
+
+  @DeleteMapping("/dog/{id}")
+  public String deleteDog(@PathVariable int id) {
+    return "DELETE Dog with ID : " + id;
+  }
+
+  @PostMapping("/dog")
+  public String addDog() {
+    return "Add Dog";
+  }
+
+  @PutMapping("/dog/{id}")
+  public String modifyDog(@PathVariable int id) {
+    return "Modify Dog";
+  }
+
+
+}
