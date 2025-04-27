0bb2005e4b4a67162ed94007920b95b4d9e62cd8
Fonzy57 <traben@hotmail.fr>
2025-04-27 16:33:54 +0200

    Add JsonView for User and add created at and updated at properties

--- src/main/java/com/k9club/api/model/User.java
+++ src/main/java/com/k9club/api/model/User.java
@@ -1,35 +1,48 @@
 package com.k9club.api.model;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.k9club.api.model.enums.UserRole;
+import com.k9club.api.views.ViewUserAdmin;
+import com.k9club.api.views.ViewUserSuperAdmin;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @JsonView(ViewUserAdmin.class)
   protected Long id;
 
   @NotBlank
   @Column(nullable = false)
   @Length(min = 3, max = 100)
+  @JsonView(ViewUserAdmin.class)
   protected String firstname;
 
   @NotBlank
   @Column(nullable = false)
   @Length(min = 3, max = 100)
+  @JsonView(ViewUserAdmin.class)
   protected String lastname;
 
   @NotBlank
   @Email
   @Column(unique = true, nullable = false)
+  @JsonView(ViewUserAdmin.class)
   protected String email;
 
   @NotBlank
@@ -37,6 +50,16 @@ public class User {
   protected String password;
 
   @Enumerated(EnumType.STRING)
-  @Column(nullable = false, columnDefinition = "ENUM('ADMIN','COACH', 'USER')")
+  @Column(nullable = false, columnDefinition = "ENUM('SUPER_ADMIN','ADMIN','COACH', 'USER')")
+  @JsonView(ViewUserSuperAdmin.class)
   protected UserRole userRole;
+
+  @CreatedDate
+  @Column(updatable = false, nullable = false)
+  @JsonView(ViewUserSuperAdmin.class)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @JsonView(ViewUserSuperAdmin.class)
+  private LocalDateTime updatedAt;
 }
