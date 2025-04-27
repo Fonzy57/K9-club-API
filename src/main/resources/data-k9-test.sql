46cf3983e1b8d39876a01ab0360edd08ffb01cef
Fonzy57 <traben@hotmail.fr>
2025-04-27 16:38:47 +0200

    Change SQL Insert query for user

--- src/main/resources/data-k9-test.sql
+++ src/main/resources/data-k9-test.sql
@@ -4,8 +4,15 @@ VALUES ('Rex', '2020-05-10', 'Male', '2024-04-10 14:30:00'),
        ('Néo', '2025-01-01', NULL, '2024-04-12 09:45:00');
 -- exemple avec genre NULL
 
-# The password is "123456"
-INSERT INTO user (firstname, lastname, email, password, user_role)
-VALUES ('Steph', 'Admin', 'admin@k9club.fr', '$2a$10$UT44bkoGz/wOFFXrAKijOe/xu3W1uPTAEjv6dZx6cj0NEN2o7JCDO', 'ADMIN'),
-       ('Victor', 'Coach', 'coach@k9club.fr', '$2a$10$UT44bkoGz/wOFFXrAKijOe/xu3W1uPTAEjv6dZx6cj0NEN2o7JCDO', 'COACH'),
-       ('Tetiana', 'User', 'user@k9club.fr', '$2a$10$UT44bkoGz/wOFFXrAKijOe/xu3W1uPTAEjv6dZx6cj0NEN2o7JCDO', 'USER');
+-- The password is "123456"
+INSERT INTO user (firstname, lastname, email, password, user_role, created_at, updated_at)
+VALUES ('Steph', 'Super Admin', 'super-admin@k9club.fr', '$2a$10$UT44bkoGz/wOFFXrAKijOe/xu3W1uPTAEjv6dZx6cj0NEN2o7JCDO',
+        'SUPER_ADMIN',
+        NOW(), NOW()),
+       ('Hubert', 'Admin', 'admin@k9club.fr', '$2a$10$UT44bkoGz/wOFFXrAKijOe/xu3W1uPTAEjv6dZx6cj0NEN2o7JCDO', 'ADMIN',
+        NOW(), NOW()),
+       ('Victor', 'Coach', 'coach@k9club.fr', '$2a$10$UT44bkoGz/wOFFXrAKijOe/xu3W1uPTAEjv6dZx6cj0NEN2o7JCDO', 'COACH',
+        NOW(), NOW()),
+       ('Tetiana', 'User', 'user@k9club.fr', '$2a$10$UT44bkoGz/wOFFXrAKijOe/xu3W1uPTAEjv6dZx6cj0NEN2o7JCDO', 'USER',
+        NOW(), NOW());
+
