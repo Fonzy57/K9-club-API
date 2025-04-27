dea582ac0ea732dec7f314b7af4f6a6d9a2bfb30
Fonzy57 <traben@hotmail.fr>
2025-04-27 09:34:06 +0200

    Add User DAO

---
+++ src/main/java/com/k9club/api/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.k9club.api.dao;
+
+import com.k9club.api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserDao extends JpaRepository<User, Long> {
+  Optional<User> findByEmail(String email);
+}
