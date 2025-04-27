d8172e04db538a941eec8e0c22bbb894193ff5a4
Fonzy57 <traben@hotmail.fr>
2025-04-27 08:56:23 +0200

    Add user model

---
+++ src/main/java/com/k9club/api/model/User.java
@@ -0,0 +1,43 @@
+package com.k9club.api.model;
+
+import com.k9club.api.security.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@Setter
+@Entity
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @NotBlank
+  protected Long id;
+
+  @NotBlank
+  @Column(nullable = false)
+  @Length(min = 3, max = 100)
+  protected String firstname;
+
+  @NotBlank
+  @Column(nullable = false)
+  @Length(min = 3, max = 100)
+  protected String lastname;
+
+  @NotBlank
+  @Email
+  @Column(unique = true, nullable = false)
+  protected String email;
+
+  @NotBlank
+  @Column(nullable = false)
+  protected String password;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false, columnDefinition = "ENUM('ADMIN','COACH', 'USER')")
+  protected Role role;
+}
