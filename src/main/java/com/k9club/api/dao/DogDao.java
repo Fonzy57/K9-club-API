c93375e8d00149737daab8edf32a9fd518ef1ecb
Fonzy57 <traben@hotmail.fr>
2025-05-08 15:04:37 +0200

    Fix Dog dao

--- src/main/java/com/k9club/api/dao/DogDao.java
+++ src/main/java/com/k9club/api/dao/DogDao.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DogDao extends JpaRepository<Dog, Integer> {
+public interface DogDao extends JpaRepository<Dog, Long> {
 }
