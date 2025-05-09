0ac0af10768f037d44bb9738172178933cc27ce1
Fonzy57 <traben@hotmail.fr>
2025-05-09 11:13:28 +0200

    Add new file for user views

---
+++ src/main/java/com/k9club/api/views/ViewUser.java
@@ -0,0 +1,12 @@
+package com.k9club.api.views;
+
+public class ViewUser {
+  public static class Owner {
+  }
+
+  public static class Admin extends Owner {
+  }
+
+  public static class SuperAdmin extends Admin {
+  }
+}
