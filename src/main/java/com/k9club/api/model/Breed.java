031b3bd4a53cd735518d22062f207576db107c4e
Fonzy57 <traben@hotmail.fr>
2025-05-18 10:52:34 +0200

    Fix breed model

--- src/main/java/com/k9club/api/model/Breed.java
+++ src/main/java/com/k9club/api/model/Breed.java
@@ -1,8 +1,8 @@
 package com.k9club.api.model;
 
-import com.fasterxml.jackson.annotation.JsonView;
-import com.k9club.api.views.ViewUser;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -13,7 +13,7 @@ import java.util.List;
 /**
  * JPA entity representing a dog breed in the K9 Club application.
  * <p>
- * Stores the breed name and a list of associated Dog entities.
+ * Stores the unique breed name and the list of associated Dog entities.
  */
 @Getter
 @Setter
@@ -21,30 +21,34 @@ import java.util.List;
 @EntityListeners(AuditingEntityListener.class)
 public class Breed {
 
-  // TODO FAIRE LE BREED CONTROLLER
+  // TODO FAIRE LES JSON VIEWS
 
   /**
    * Primary key identifier for the breed.
    */
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @JsonView(ViewUser.Owner.class)
   protected Long id;
 
   /**
-   * Name of the breed; must not be null and up to 100 characters.
+   * Unique name of the breed.
+   * <p>
+   * Must not be blank and must contain between 5 and 100 characters.
+   * Validation messages:
+   * - "Le nom est obligatoire" if blank
+   * - "Le nom doit contenir entre 5 et 100 caractères" if outside length bounds
    */
-  @Column(nullable = false, length = 100)
-  @JsonView(ViewUser.Owner.class)
+  @NotBlank(message = "Le nom est obligatoire")
+  @Size(min = 5, max = 100, message = "La nom doit contenir entre 5 et 100 caractères")
+  @Column(nullable = false, length = 100, unique = true)
   protected String name;
 
   /**
    * List of dogs belonging to this breed.
    * <p>
-   * One-to-many relationship with Dog; orphanRemoval ensures that
-   * deleting a breed will remove its associated dogs.
+   * One-to-many relationship with {@link Dog}; orphanRemoval is not enabled here
+   * to preserve historic data if a breed is removed.
    */
-  @OneToMany(mappedBy = "breed", orphanRemoval = true)
-  @JsonView(ViewUser.Owner.class)
+  @OneToMany(mappedBy = "breed")
   protected List<Dog> dogs = new ArrayList<>();
 }
