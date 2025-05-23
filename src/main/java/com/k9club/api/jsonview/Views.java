96259f27a934fe2680393498d7dc3f9184ec2e56
Fonzy57 <traben@hotmail.fr>
2025-05-23 09:53:34 +0200

    "Refactor user view hierarchy: rename and reorganize classes under jsonview package."

---
+++ src/main/java/com/k9club/api/jsonview/Views.java
@@ -0,0 +1,13 @@
+package com.k9club.api.jsonview;
+
+public class Views {
+
+  public static class Public {
+  }
+
+  public static class Coach extends Public {
+  }
+
+  public static class Admin extends Coach {
+  }
+}
