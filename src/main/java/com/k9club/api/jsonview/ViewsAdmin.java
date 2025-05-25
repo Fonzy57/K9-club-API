090f2e426aedda5b205789cbcf2a38d20cc5ff50
Fonzy57 <traben@hotmail.fr>
2025-05-25 11:45:36 +0200

    "Add JSON view classes for Owner, Coach, and enhance Admin views hierarchy."

--- src/main/java/com/k9club/api/jsonview/ViewsAdmin.java
+++ src/main/java/com/k9club/api/jsonview/ViewsAdmin.java
@@ -2,6 +2,21 @@ package com.k9club.api.jsonview;
 
 public class ViewsAdmin {
 
-  public static class DogsInfo {
+  public static class Basic {
+  }
+
+  public static class AgeRangeInfo extends Basic {
+  }
+
+  public static class DogsInfo extends Basic {
+  }
+
+  public static class CourseInfo extends Basic {
+  }
+
+  public static class CourseRegistrationInfo extends Basic {
+  }
+
+  public static class CourseTypeInfo extends Basic {
   }
 }
